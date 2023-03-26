d153589b72929aca586320ae596fbdec726af026
okarin2033 <60982631+okarin2033@users.noreply.github.com>
2023-03-26 16:30:55 +0300

    fixedApiLeak

--- src/main/java/ru/openai/api/Service/Config/Configuration.java
+++ src/main/java/ru/openai/api/Service/Config/Configuration.java
@@ -4,6 +4,6 @@ import lombok.Getter;
 
 public class Configuration {
     @Getter
-    final private static String API_KEY = "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";
+    final private static String API_KEY = "myKey";
 
 }
